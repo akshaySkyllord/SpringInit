da532c47b66c98ff93e30fa9827cf88339597b0d
Akshay V Pange <GS-2219@GSLAB.COM>
2019-07-12 15:12:55 +0530

    Admincontroller change

--- src/main/java/com/springboot/connector/AdminController.java
+++ src/main/java/com/springboot/connector/AdminController.java
@@ -1,8 +1,8 @@
 package com.springboot.connector;
 
-import javax.persistence.NoResultException;
-import javax.servlet.http.HttpSession;
-
+import com.springboot.pojo.Employee;
+import com.springboot.pojo.Login;
+import com.springboot.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,111 +12,94 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.springboot.pojo.Employee;
-import com.springboot.pojo.Login;
-import com.springboot.service.AdminService;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
 @RequestMapping("/system")
-public class AdminController 
+public class AdminController {
+    @Autowired
+    private AdminService service;
 
-{
-	@Autowired
-	private AdminService service;
-
-	public AdminController() 
-	{
-		System.out.println("inside constructor of"+getClass().getName());
-	}
+    public AdminController() {
+        System.out.println("inside constructor of" + getClass().getName());
+    }
 
 
-	@GetMapping("/welcome")
-	public String showwelcome()
-	{
-		System.out.println("in welcome form ");
-		return "welcome";
-	}
+    @GetMapping("/welcome")
+    public String showwelcome() {
+        System.out.println("in welcome form ");
+        return "welcome";
+    }
 
 
-	@GetMapping("/register")
-	public String showRegForm(Employee m)
-	{
-		System.out.println("in show registration");
-		return "register";
-	}
-	@PostMapping("/register")
-	public String processRegForm(Employee m,RedirectAttributes flashMap,Model map)
-	{
-		System.out.println("in process reg detail form "+m);
-		flashMap.addFlashAttribute("status",service.registerUser(m));
-		map.addAttribute("member_list", service.listEmployee());
-		return "memberlogin";
-	}
+    @GetMapping("/register")
+    public String showRegForm(Employee m) {
+        System.out.println("in show registration");
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegForm(Employee m, RedirectAttributes flashMap, Model map) {
+        System.out.println("in process reg detail form " + m);
+        flashMap.addFlashAttribute("status", service.registerUser(m));
+        map.addAttribute("member_list", service.listEmployee());
+        return "memberlogin";
+    }
 
 
-
-
-
-
-
-	@GetMapping("/member")
-	public String showSocietyMember(Employee m,RedirectAttributes flashMap,Model map)
-	{
+    @GetMapping("/member")
+    public String showSocietyMember(Employee m, RedirectAttributes flashMap, Model map) {
 		/*System.out.println("in society detail "+member);
 							return "member";*/
 
-		System.out.println("display member details "+m);
-		flashMap.addFlashAttribute("status",service.registerUser(m));
-		map.addAttribute("society_list", service.listEmployee());
-		return "/member";
+        System.out.println("display member details " + m);
+        flashMap.addFlashAttribute("status", service.registerUser(m));
+        map.addAttribute("society_list", service.listEmployee());
+        return "/member";
 
 
-	}
+    }
 
 
-	@GetMapping("/logout")
-	public String adminLogout(HttpSession hs)
-	{
-		System.out.println("in logout");
-		hs.invalidate();
-		return "redirect:login";
-	}
+    @GetMapping("/logout")
+    public String adminLogout(HttpSession hs) {
+        System.out.println("in logout");
+        hs.invalidate();
+        return "redirect:login";
+    }
 
 
-	@GetMapping("/login")
-	public String showLoginForm()
-	{
-		System.out.println("In show login form");
-		return "/login";
-	}
-	@PostMapping("/login")
-	public String processLoginForm(@RequestParam String username,@RequestParam String password, Model map, HttpSession hs,RedirectAttributes flashMap) 
-	{
+    @GetMapping("/login")
+    public String showLoginForm() {
+        System.out.println("In show login form");
+        return "/login";
+    }
 
-		System.out.println("in process login form " + username + " " + password);
-		Login l=null;
-		try {
-			l = service.authenticateUser(username, password);
-			//valid login
-			if(l.getUsername().equals("admin")) 
-			{
-				//add validated user dtls under session scope
-				hs.setAttribute("admin_dtls", l);//v ---DETACHED
-				flashMap.addFlashAttribute("status","admin Login Successful");
-				return "/welcome";
-			}
+    @PostMapping("/login")
+    public String processLoginForm(@RequestParam String username, @RequestParam String password, Model map, HttpSession hs, RedirectAttributes flashMap) {
 
-			//hs.setAttribute("user_dtls", l);//v ---DETACHED
-			//flashMap.addFlashAttribute("status","user Login Successful");
+        System.out.println("in process login form " + username + " " + password);
+        Login l = null;
+        try {
+            l = service.authenticateUser(username, password);
+            //valid login
+            if (l.getUsername().equals("admin")) {
+                //add validated user dtls under session scope
+                hs.setAttribute("admin_dtls", l);//v ---DETACHED
+                flashMap.addFlashAttribute("status", "admin Login Successful");
+                return "/welcome";
+            }
 
-			return "welcome";	
+            //hs.setAttribute("user_dtls", l);//v ---DETACHED
+            //flashMap.addFlashAttribute("status","user Login Successful");
 
-		} catch (Exception e)
-		{
-			map.addAttribute("status", "Invalid Login , Please try again..");
-			return "login";
-		}
+            return "welcome";
 
-	}
+        } catch (Exception e) {
+            map.addAttribute("status", "Invalid Login , Please try again..");
+            return "login";
+        }
+
+    }
 }
